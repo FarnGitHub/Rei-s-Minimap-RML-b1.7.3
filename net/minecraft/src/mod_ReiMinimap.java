e51c31c56824f1707b6580f69bdf3539a62001af
Farn <93525542+FarnGitHub@users.noreply.github.com>
2026-04-15 08:14:21 +0700

    move WayPoint renderer

--- net/minecraft/src/mod_ReiMinimap.java
+++ net/minecraft/src/mod_ReiMinimap.java
@@ -2,6 +2,10 @@ package net.minecraft.src;
 
 import net.minecraft.client.Minecraft;
 import reifnsk.minimap.ReiMinimap;
+import reifnsk.minimap.WaypointEntity;
+import reifnsk.minimap.WaypointEntityRender;
+
+import java.util.Map;
 
 public class mod_ReiMinimap extends BaseMod{
     @Override
@@ -13,8 +17,13 @@ public class mod_ReiMinimap extends BaseMod{
         ModLoader.SetInGameHook(this, true, false);
     }
 
-    public boolean OnTickInGame(Minecraft var1) {
-        ReiMinimap.instance.onTickInGame(var1);
+    public boolean OnTickInGame(Minecraft mc) {
+        ReiMinimap.instance.onTickInGame(mc);
         return true;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public void AddRenderer(Map entityRendererMap) {
+        entityRendererMap.put(WaypointEntity.class, new WaypointEntityRender(ModLoader.getMinecraftInstance()));
     }
 }
